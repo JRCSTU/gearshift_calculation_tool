91234bdca60b0d464c3a534c38b50482dee0575e
Andrés Laverde Marin <Andres.LAVERDE-MARIN@ext.ec.europa.eu>
2020-11-30 09:41:39 +0100

    Added the excel writer dispacher

--- gearshift/core/__init__.py
+++ gearshift/core/__init__.py
@@ -25,6 +25,7 @@ import schedula as sh
 import os.path as osp
 from .load import dsp as _load
 from .model import dsp as _model
+from .write import dsp as _write
 
 log = logging.getLogger(__name__)
 
@@ -181,7 +182,7 @@ def _obtain_inputs(case, base):
 def run_model(base, model):
     from tqdm import tqdm
 
-    sol, input, case = {}, {}, base["case"]
+    sol, input, case = [], {}, base["case"]
 
     with tqdm(total=len(list(case.iterrows()))) as pbar:
         for index, row in case.iterrows():
@@ -189,8 +190,21 @@ def run_model(base, model):
 
             input = _obtain_inputs(row, base)
 
-            sol[index] = model(dict(input))
+            sol_case = model(dict(input))
+            dict_case = {"Case": row.to_dict()["case"], "NoOfGears": sol_case['shift_poits']['NoOfGearsFinal']}
+            for k, v in sh.stack_nested_keys(sol_case.get("shift_poits", {}), depth=2):
+                if len(k) >= 2:
+                    dict_case[str(k[1])] = v
+
+            sol.append(dict_case)
 
             pbar.update(1)
 
     return sol
+
+
+dsp.add_dispatcher(
+    dsp=_write,
+    inputs=("output_folder", "sol", "timestamp", "output_format"),
+    outputs=("output_file_name", sh.SINK),
+)
