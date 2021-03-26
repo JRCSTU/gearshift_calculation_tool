a3d36d67e749dc8510ace71f0b0e97a813389749
andreslaverdemarin <andreslaverdemarin@hotmail.com>
2021-03-26 15:48:35 +0100

    update load method to call the dice read input template

--- gearshift/core/load/__init__.py
+++ gearshift/core/load/__init__.py
@@ -24,6 +24,7 @@ import logging
 import pandas as pd
 import schedula as sh
 from .excel import parse_excel_file
+from .excel_dice import load_dice_file
 
 log = logging.getLogger(__name__)
 
@@ -110,113 +111,12 @@ def check_file_format_co2mpas(input_file_name, *args, ext=(".xlsx",)):
         return input_file_name.lower().endswith(ext)
 
 
-def _mergeDict(dict1, dict2):
-    """ Merge dictionaries and keep values of common keys in list"""
-    for key in dict2.keys():
-        if key in dict1:
-            dict1[key].update(dict2[key])
-        else:
-            dict1[key] = dict2[key]
-
-    return dict1
-
-
-@sh.add_function(
-    dsp,
-    inputs_kwargs=True,
+dsp.add_function(
+    function=load_dice_file,
+    inputs=["input_file_name"],
     outputs=["raw_data"],
     input_domain=check_file_format_co2mpas,
 )
-def load_excel_file(input_file_name):
-    from co2mpas.core.load import dsp as _load
-
-    data = _load({"input_file_name": input_file_name})
-
-    input_data = sh.get_nested_dicts(
-        data,
-        "raw_data",
-        "base",
-        "input",
-        "calibration",
-    )
-
-    target_data = sh.get_nested_dicts(
-        data,
-        "raw_data",
-        "base",
-        "target",
-        "calibration",
-    )
-
-    keys_case = (
-        "capped_velocity",
-        "hs_n_min1",
-        "hs_n_min12",
-        "hs_n_min2d",
-        "hs_n_min2",
-        "hs_n_min3",
-        "hs_n_min3a",
-        "hs_n_min3d",
-        "hs_n_min3as",
-        "hs_n_min3ds",
-        "hs_t_start",
-        "hs_supp0",
-        "hs_excl1",
-        "hs_autom",
-        "hs_n_lim",
-        "asm_margin",
-    )
-
-    final_input = _mergeDict(input_data, target_data)
-
-    case = {
-        cycle: sh.selector(keys_case, d, allow_miss=True)
-        for cycle, d in final_input.items()
-    }
-    case = dict(filter(lambda x: bool(x[1]) != False, case.items()))
-
-    keys_vehicle = (
-        "engine_max_power",
-        "engine_speed_at_max_power",
-        "idle_engine_speed_median",
-        "vehicle_mass",
-        "f0",
-        "f1",
-        "f2",
-    )
-    vehicle = {
-        cycle: sh.selector(keys_vehicle, d, allow_miss=True)
-        for cycle, d in final_input.items()
-    }
-    vehicle = dict(filter(lambda x: bool(x[1]) != False, vehicle.items()))
-
-    keys_engine = {
-        "full_load_speeds",
-        "full_load_powers",
-    }
-    engine = {
-        cycle: sh.selector(keys_engine, d, allow_miss=True)
-        for cycle, d in final_input.items()
-    }
-    engine = dict(filter(lambda x: bool(x[1]) != False, engine.items()))
-
-    keys_speed_phase_data = {"times", "obd_velocities"}
-    speed_phase_data = {
-        cycle: sh.selector(keys_speed_phase_data, d, allow_miss=True)
-        for cycle, d in final_input.items()
-    }
-    speed_phase_data = dict(
-        filter(lambda x: bool(x[1]) != False, speed_phase_data.items())
-    )
-
-    raw_data = {
-        "case": case,
-        "vehicle": vehicle,
-        "engine": engine,
-        "speed_phase_data": speed_phase_data,
-    }
-
-    return raw_data
 
 
 def _load_speed_phase_data():
