3c86ed17455b0fff3a68ae44a79a54a59f55bcbb
AndresLaverdeMarin <andreslaverdemarin@hotmail.com>
2021-06-16 11:23:33 +0200

    update(setup): Update header with the new licence definition.

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
-# Copyright 2015-2019 European Commission (JRC);
-# Licensed under the EUPL (the 'Licence');
+# -*- coding: utf-8 -*-
+#
+# Copyright 2013-2021 European Commission (JRC);
+# Licensed under the EUPL, Version 1.2 or – as soon they will be approved by the European Commission
+# – subsequent versions of the EUPL (the "Licence");
+#
 # You may not use this work except in compliance with the Licence.
-# You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+# You may obtain a copy of the Licence at: [11:17] MITSIARA Athina (JRC-ISPRA-EXT)
+#     https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
 """
 GEARSHIFT setup.
 """
