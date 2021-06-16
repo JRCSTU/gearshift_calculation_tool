09731295c5246a2ca853862268efb02f6c226e6b
AndresLaverdeMarin <andreslaverdemarin@hotmail.com>
2021-06-16 11:35:00 +0200

    update(excel): Update header with the new licence definition.

--- gearshift/core/load/excel_dice.py
+++ gearshift/core/load/excel_dice.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2015-2020 European Commission (JRC);
-# Licensed under the EUPL (the 'Licence');
+# Copyright 2013-2021 European Commission (JRC);
+# Licensed under the EUPL, Version 1.2 or – as soon they will be approved by the European Commission
+# – subsequent versions of the EUPL (the "Licence");
+#
 # You may not use this work except in compliance with the Licence.
-# You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+# You may obtain a copy of the Licence at: https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
 """
 Functions to read inputs from dice excel template.
 """
